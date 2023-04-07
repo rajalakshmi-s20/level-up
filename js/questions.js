37cf090e96f6ed79fda84bc9db6e24dffc94fdae
RAJALAKSHMI.S <rajalakshmi.sarangan20@gmail.com>
2023-04-07 21:29:17 +0530

    final

---
+++ js/questions.js
@@ -0,0 +1,170 @@
+
+// creating an array and passing the number, questions, options and answers
+
+let questions = [
+    {
+    numb: 1,
+    question: "Which keyword is used to prevent any changes in the variable within a C program?",
+    answer: "Const",
+    options: [
+      "Immutable",
+      "Mutable",  
+      "Const",
+      "Volatile"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is an example of iteration in C?",
+    answer: "All of the mentioned",
+    options: [
+      "for",
+      "While",
+      "do-While",
+      "All of the mentioned"
+    ]
+  },
+    {
+    numb: 3,
+    question: "C preprocessors can have compiler specific features.",
+    answer: "True",
+    options: [
+      "True",
+      "False",
+      "Depends on the standard",
+      "Depends on the platform"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The C preprocessors are specified with _______ symbol.",
+    answer: "#",
+    options: [
+      "#",
+      "$",
+      "*",
+      "&"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How many number of pointers does C have against a pointer variable declaration?",
+    answer: "No limits",
+    options: [
+      "7",
+      "127",
+      "255",
+      "No limits"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which symbol is used as a statement terminator in C?",
+    answer: ";",
+    options: [
+      "!",
+      "#",
+      "~",
+      ";"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which of the following is an exit controlled loop?",
+    answer: "do-While loop",
+    options: [
+      "While loop",
+      "for loop",
+      "do-While loop",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 8,
+    question: "String constants should be enclosed between",
+    answer: "Double Quotes",
+    options: [
+      "Single Quotes",
+      "Double Quotes",
+      "Both a and b",
+      "None of these"
+    ]
+  },
+  {
+    numb: 9,
+    question: "What will be the maximum size of a float variable in c?",
+    answer: "4 bytes",
+    options: [
+      "1 byte",
+      "2 bytes",
+      "4 bytes",
+      "8 bytes"
+    ]
+  },
+  {
+    numb: 10,
+    question: "The equality operator is represented in c as",
+    answer: "==",
+    options: [
+      ":=",
+      "=",
+      "!=",
+      "=="
+    ]
+  },
+  {
+    numb: 11,
+    question: "The operator && is an example for _______ operator",
+    answer: "Logical",
+    options: [
+      "Assignment",
+      "Increment",
+      "Logical",
+      "Rational"
+    ]
+  },
+  {
+    numb: 12,
+    question: "Which operator has the highest priority in c?",
+    answer: "++",
+    options: [
+      "++",
+      "%",
+      "+",
+      "||"
+    ]
+  },
+  {
+    numb: 13,
+    question: "Explicit type conversion is known as ",
+    answer: "Casting",
+    options: [
+      "Casting",
+      "Conversion",
+      "Disjunction",
+      "Separation"
+    ]
+  },
+  {
+    numb: 14,
+    question: "Which pair of functions below are used for single character I/O?",
+    answer: "Getchar() and putchar()",
+    options: [
+      "Getchar() and putchar()",
+      "Scanf() and printf()",
+      "Input() and Output()",
+      "None of these"
+    ]
+  },
+  {
+    numb: 15,
+    question: "Array subscripts in C always start at ",
+    answer: "0",
+    options: [
+      "1",
+      "-1",
+      "0",
+      "Value provided by the user"
+    ]
+  },
+];
